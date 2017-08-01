fe7c1c695692f73abe789102cf159b6e9ff99b0d
Snigdha Rani Sahu <snigdharani@hyperthinksys.com>
2017-08-01 13:03:52 +0530

    menubar customised

--- ECommeia/Pods/PageMenu/Classes/CAPSPageMenuConfiguration.swift
+++ ECommeia/Pods/PageMenu/Classes/CAPSPageMenuConfiguration.swift
@@ -9,20 +9,20 @@
 import UIKit
 
 public class CAPSPageMenuConfiguration {
-    open var menuHeight : CGFloat = 34.0
+    open var menuHeight : CGFloat = 50.0
     open var menuMargin : CGFloat = 15.0
     open var menuItemWidth : CGFloat = 111.0
     open var selectionIndicatorHeight : CGFloat = 3.0
     open var scrollAnimationDurationOnMenuItemTap : Int = 500 // Millisecons
     open var selectionIndicatorColor : UIColor = UIColor.white
     open var selectedMenuItemLabelColor : UIColor = UIColor.white
-    open var unselectedMenuItemLabelColor : UIColor = UIColor.blue
-    open var scrollMenuBackgroundColor : UIColor = UIColor.lightGray
+    open var unselectedMenuItemLabelColor : UIColor = UIColor(white: 1.0, alpha: 0.3)
+    open var scrollMenuBackgroundColor : UIColor = UIColor(red: 146/255, green: 105/255, blue: 162/255, alpha: 1.0)
     open var viewBackgroundColor : UIColor = UIColor.white
-    open var bottomMenuHairlineColor : UIColor = UIColor.red
-    open var menuItemSeparatorColor : UIColor = UIColor.blue
+    open var bottomMenuHairlineColor : UIColor = UIColor.lightGray
+    open var menuItemSeparatorColor : UIColor = UIColor.clear
     
-    open var menuItemFont : UIFont = UIFont.systemFont(ofSize: 15.0)
+    open var menuItemFont : UIFont = UIFont(name: "HelveticaNeue-Bold", size: 15.0)!
     open var menuItemSeparatorPercentageHeight : CGFloat = 0.2
     open var menuItemSeparatorWidth : CGFloat = 0.5
     open var menuItemSeparatorRoundEdges : Bool = false
